848493d7c0c53982c001e1b16d41c0105905b591
Malek TRABELSI <malek.isims88@gmail.com>
2021-03-31 18:58:54 +0200

    Add test

--- Tests/EasyLogTests/EasyLogTests.swift
+++ Tests/EasyLogTests/EasyLogTests.swift
@@ -1,13 +1,20 @@
 import XCTest
+import EasyLog
 
 final class EasyLogTests: XCTestCase {
+    let logger: Logger!
+    override func setUpWithError() throws {
+        logger = Logger()
+    }
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-//        XCTAssertNotNil(Logger())
+        XCTAssertNotNil(logger)
     }
-
+    override func tearDownWithError() throws {
+        logger = nil
+    }
     static var allTests = [
         ("testExample", testExample),
     ]
